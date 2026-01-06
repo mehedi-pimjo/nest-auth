4cd8edf6dc1d570b26b7683439afa9e22a3dd82b
Mehedi Pimjo <mehedi.pimjo@gmail.com>
2026-01-06 13:37:41 +0600

    remove user module

--- src/users/users.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-
-@Injectable()
-export class UsersService {
-  constructor(private prisma: PrismaService) {}
-  
-  
-}
