2e9efc099f531e6736f5586c4df11a73e3c6b856
Mehedi Pimjo <mehedi.pimjo@gmail.com>
2026-01-06 13:39:19 +0600

    generate user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
