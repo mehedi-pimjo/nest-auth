b125fcfc8c750072191ef3bb97354c3bfd3cab24
Mehedi Pimjo <mehedi.pimjo@gmail.com>
2026-01-06 11:13:03 +0600

    PrismaService class inherits from PrismaClient class

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from 'src/generated/prisma/client';
 
 @Injectable()
-export class PrismaService {
+export class PrismaService extends PrismaClient {
   constructor (private config: ConfigService) {
     const url = this.config.getOrThrow<string>('DATABASE_URL');
   }
