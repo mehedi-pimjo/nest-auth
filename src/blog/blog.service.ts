d7f27f9f843a7fac5730364980fbbfd1df26caef
Mehedi Pimjo <mehedi.pimjo@gmail.com>
2026-01-08 11:44:33 +0600

    add createBlog dto & service

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { JwtPayload } from 'src/common/types/jwt-payload.interface';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBlogDto } from './dto/create-blog.dto';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(user: JwtPayload, createData: CreateBlogDto) {
+    const createdBlog = await this.prisma.blog.create({
+      data: {
+        title: createData.title,
+        content: createData.content,
+        authorId: user.sub,
+      },
+      include: {
+        author: {
+          omit: {
+            password: true,
+            role: true,
+          },
+        },
+      },
+    });
+
+    return createdBlog;
+  }
+}
