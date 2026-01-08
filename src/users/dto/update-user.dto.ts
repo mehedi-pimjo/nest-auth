d57e166fe27fbdaa6bdc906763ff3426de6d5017
Mehedi Pimjo <mehedi.pimjo@gmail.com>
2026-01-08 12:34:20 +0600

    add 'update-blog.dto.ts and update 'update-blog.dto.ts'

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,21 +1,4 @@
-import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsEmail({}, { message: 'Please enter a valid email address' })
-  @IsOptional()
-  @MaxLength(255, { message: 'Email is too long' })
-  email?: string;
-
-  @IsString()
-  @IsOptional()
-  @MaxLength(100, { message: 'Name is too long (max 100 characters)' })
-  name?: string;
-
-  @IsString()
-  @IsOptional()
-  @MinLength(4, { message: 'Password must be at least 4 characters long' })
-  @MaxLength(128, { message: 'Password is too long' })
-  password?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
